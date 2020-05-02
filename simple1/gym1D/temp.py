deb2769b99346ac93c6a5a80130af036e85723a9
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-02 10:16:13 -0400

    python scripts

---
+++ simple1/gym1D/temp.py
@@ -0,0 +1,14 @@
+import numpy as np
+import gym
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+
+import rl
+print(rl.__file__)
