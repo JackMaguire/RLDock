855083afcce5252e2dd1039dbc291fe83dbdc109
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-02 10:45:09 -0400

    file management

--- simple1/deep_gym1D/gym1D/__init__.py
+++ simple1/deep_gym1D/gym1D/__init__.py
@@ -6,7 +6,5 @@ logger = logging.getLogger(__name__)
 register(
     id='Gym1D-v0',
     entry_point='gym1D.envs:Gym1DEnv',
-    timestep_limit=1000,
-    reward_threshold=1.0,
-    nondeterministic = True,
+    max_episode_steps=200,
 )
