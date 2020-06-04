e864992e02ef1238ae027bc102a9524708b14239
Jack Maguire <jackmaguire1444@gmail.com>
2020-06-04 14:42:41 -0400

    run master and worker

--- run_distributed/run.py
+++ run_distributed/run.py
@@ -1,17 +1,26 @@
 from mpi4py import MPI
 
+from run_master import *
+
 comm = MPI.COMM_WORLD
 nprocs = comm.Get_size()
 rank = comm.Get_rank()
 
-def run_master():
-    pass
+def run_slave( comm, rank ):
 
-def run_worker():
-    pass
+    pose = whatever()
+
+    while True:
+        six_dofs = comm.recv( source=0, tag=11 )
+        if is_kill_signal( six_dofs ):
+            comm.send( 0, dest=0, tag=0 )
+            break
+        score, pose_filename = score_six_dofs( six_dofs )
+        bundle = [ score, pose_filename ]
+        comm.send( bundle, dest=0, tag=11 )
 
 if rank == 0:
-    run_master()
+    run_master( comm, nprocs, rank )
 else:
-    run_slave()
+    run_slave( comm, rank )
 
