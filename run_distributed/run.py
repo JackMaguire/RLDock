65323024c33b6433a1a82aad492f43408af06665
Jack Maguire <jackmaguire1444@gmail.com>
2020-06-12 15:07:33 +0000

    updating templates

--- run_distributed/run.py
+++ run_distributed/run.py
@@ -1,26 +1,44 @@
 from mpi4py import MPI
-
 from run_master import *
+import argparse
+#import nevergrad as ng
+
+parser = argparse.ArgumentParser(description='test_narrowness.py')
+parser.add_argument('--opt', help='optimizer to use', required=True )
+parser.add_argument('--budget', help='budget for optimizer', required=True, type=int )
+args = parser.parse_args()
+
+print( args.opt )
+
 
 comm = MPI.COMM_WORLD
 nprocs = comm.Get_size()
 rank = comm.Get_rank()
 
+def scorer(x):
+    mysum = 0
+    for i in x:
+        mysum += i*i
+    return mysum
+
 def run_slave( comm, rank ):
 
-    pose = get_starting_pose() #TODO IVY
-
     while True:
-        six_dofs = comm.recv( source=0, tag=11 )
-        if is_kill_signal( six_dofs ): #TODO JACK
+        status = MPI.Status()
+        six_dofs = comm.recv( source=0, tag=MPI.ANY_TAG, status=status )
+        if status.Get_tag() == 0:
             comm.send( 0, dest=0, tag=0 )
+            print( "Dying" )
             break
-        score, pose_filename = score_six_dofs( six_dofs ) #TODO IVY/BRIAN
-        bundle = [ score, pose_filename ]
-        comm.send( bundle, dest=0, tag=11 )
+
+        #print( "Running", six_dofs.value )
+        score = scorer( six_dofs.value )
+        pose_filename = "test"
+        bundle = [ six_dofs, score, pose_filename ]
+        comm.send( bundle, dest=0, tag=1 )
 
 if rank == 0:
-    run_master( comm, nprocs, rank )
+    run_master( comm=comm, nprocs=nprocs, rank=rank, opt=args.opt, budget=args.budget )
 else:
     run_slave( comm, rank )
 
--- run_distributed/run_master.py
+++ run_distributed/run_master.py
@@ -1,64 +1,70 @@
 from mpi4py import MPI
-from sets import Set
+#from sets import Set
+import nevergrad as ng
 
-def send_job_to_node( six_dofs, node ):
-    comm.send( six_dofs, dest=node, tag=11 )
+def send_job_to_node( comm, six_dofs, node, tag=1 ):
+    comm.send( six_dofs, dest=node, tag=tag )
 
-def execute_kill_seq( available_nodes, working_nodes ):
-    six_dofs = get_kill_signal()
+def interpret_result( bundle ):
+    six_dofs = bundle[ 0 ]
+    score = bundle[ 1 ]
+    pose_filename = bundle[ 2 ]
+    #TODO
+
+def tell_node_to_die( comm, node ):
+    send_job_to_node( comm, "die", node, tag=0 )
+    message = comm.recv( source=node, tag=MPI.ANY_TAG )
+    if message != 0:
+        print( "Node ", node, " sent ", message, " instead of 0 upon kill" )
+    
+def execute_kill_seq( comm, available_nodes, working_nodes ):
 
     while len( available_nodes ) > 0:
         node = available_nodes.pop()
-        send_job_to_node( six_dofs, node )
-        message = comm.recv( source=node, tag=MPI.ANY_TAG )
-        if message != 0:
-            print( "Node ", node, " sent ", message, " instead of 0 upon kill" )
-
+        tell_node_to_die( comm, node )
+        
     while len( working_nodes ) > 0:
         status = MPI.Status()
-        bundle = comm.recv( source=MPI.ANY_SOURCE, tag=11, status=status )
+        bundle = comm.recv( source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status )
+        interpret_result( bundle )
+        
         source = status.Get_source()
         working_nodes.remove( source )
-        #available_nodes.add( source )
-
-        score = bundle[ 0 ]
-        pose_filename = bundle[ 1 ]
-        interpret_result( score, pose_filename )
-
-        node = source
-        send_job_to_node( six_dofs, node )
-        message = comm.recv( source=node, tag=MPI.ANY_TAG )
-        if message != 0:
-            print( "Node ", node, " sent ", message, " instead of 0 upon kill" )
-
-    exit( 0 )
-
+        tell_node_to_die( comm, source )
 
 #https://stackoverflow.com/questions/21088420/mpi4py-send-recv-with-tag
-def run_master( comm, nprocs, rank ):
-    available_nodes = Set()
+def run_master( comm, nprocs, rank, opt, budget ):
+    
+    available_nodes = set()
     for i in range( 1, nprocs ):
         available_nodes.add( i )
 
-    working_nodes = Set()
+    working_nodes = set()
+    
+    optimizer = ng.optimizers.registry[ opt ]( parametrization=6, budget=budget, num_workers=(nprocs-1) )
+    
+    for b in range( 0, budget ):
+        if len( available_nodes ) == 0:
+            #All are busy, wait for results
+            status = MPI.Status()
+            bundle = comm.recv( source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status )
+            source = status.Get_source()
+            working_nodes.remove( source )
+            available_nodes.add( source )
 
-    while True:
-        while len( available_nodes ) > 0:
-            six_dofs = get_next_sample() #TODO IVY/JACK
-            if is_kill_signal( six_dofs ): #TODO JACK
-                execute_kill_seq( available_nodes, working_nodes )
+            six_dofs = bundle[ 0 ]
+            score = bundle[ 1 ]
+            pose_filename = bundle[ 2 ]
+            optimizer.tell( six_dofs, score )
 
-            node = available_nodes.pop()
-            send_job_to_node( six_dofs, node )
-            working_nodes.add( node )
+            interpret_result( bundle )
+        #end if
+        
+        six_dofs = optimizer.ask()
 
-        #All are busy, wait for results
-        status = MPI.Status()
-        bundle = comm.recv( source=MPI.ANY_SOURCE, tag=11, status=status )
-        source = status.Get_source()
-        working_nodes.remove( source )
-        available_nodes.add( source )
-
-        score = bundle[ 0 ]
-        pose_filename = bundle[ 1 ]
-        interpret_result( score, pose_filename ) #TODO IVY/JACK
+        node = available_nodes.pop() #removes node from available_nodes
+        send_job_to_node( comm, six_dofs, node )
+        working_nodes.add( node )
+    #end for b    
+    execute_kill_seq( comm, available_nodes, working_nodes )
+    print( optimizer.provide_recommendation().value )
