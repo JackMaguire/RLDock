14dc9f2fcd6391d4d0509c5fbb13c4dcb5811e8c
Jack Maguire <jackmaguire1444@gmail.com>
2020-06-24 15:26:46 +0000

    test_pyrosetta.py

--- run_distributed/score_dofs.py
+++ run_distributed/score_dofs.py
@@ -1,7 +1,7 @@
 from pyrosetta import *
 from pyrosetta.teaching import *
-import rosetta.protocols.rigid as rigid_moves
-from rosetta.protocols.rosetta_scripts import *
+#import rosetta.protocols.rigid as rigid_moves
+from pyrosetta.rosetta.protocols.rosetta_scripts import *
 
 def magic_number_for_failed_docking_filter():
     return 1;
@@ -9,12 +9,20 @@ def magic_number_for_failed_docking_filter():
 def magic_number_for_failed_design_filter():
     return 0.5;
 
-def score_dofs( dofs ):
+def score_separate_dofs( t, r ):
+    assert( len( t ) == 3 )
+    assert( len( r ) == 3 )
+    return score_dofs( [ t[0], t[1], t[2], r[0], r[1], r[2] ] )
 
+def score_dofs( dofs ):
+    pyrosetta.init()
+    
     assert( len( dofs ) == 6 )
     
-    pose = pose_from_pdb("test_aligned_3H.pdb")   # input pdbfile
-
+    #pose = pose_from_pdb("test_aligned_3H.pdb")   # input pdbfile
+    pose = pose_from_pdb("3u3b.clean.pdb")
+    #pose = pose_from_sequence("VVV/LLLK")
+    
     # switch to centroid pose temporarily
     recover_sidechains = ReturnSidechainMover(pose)
     switch = SwitchResidueTypeSetMover("centroid")
@@ -23,7 +31,8 @@ def score_dofs( dofs ):
     #TODO setup foldtree
     
     
-    jump = pose.jump( pose.num_jumps() )
+    jump = pose.jump( pose.num_jump() )
+    jump_control = pose.jump( pose.num_jump() )
 
     #ALL ANGLES ARE IN DEGREES
     
@@ -37,19 +46,30 @@ def score_dofs( dofs ):
     d_rz = dofs[ 5 ] * R_factor
     
     #jump.set_rb_delta(Jump::ROT_X, 1, numeric::random::gaussian() * magnitude_rotation());
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.TRANS_X, 1, d_tx )
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.TRANS_Y, 1, d_ty )
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.TRANS_Z, 1, d_tz )
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.ROT_X, 1, d_rx )
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.ROT_Y, 1, d_ry )
-    jump.set_rb_delta( rosetta.core.kinematics.Jump.ROT_Z, 1, d_rz )
-
+    jump.set_rb_delta( 1, 1, d_tx )
+    jump.set_rb_delta( 2, 1, d_ty )
+    jump.set_rb_delta( 3, 1, d_tz )
+    jump.set_rb_delta( 4, 1, d_rx )
+    jump.set_rb_delta( 5, 1, d_ry )
+    jump.set_rb_delta( 6, 1, d_rz )
+    
     #Perform quick filter
     low_res_sfxn = create_score_function( "interchain_cen" )
     low_res_sfxn.set_weight(interchain_contact, 4.0 )
     low_res_sfxn.set_weight(interchain_pair, 0.0 )
     low_res_sfxn.set_weight(interchain_env, 0.0 )
-    low_res_score = low_res_sfxn.score( pose )
+
+    #Make the chains go very far away
+    jump_control.set_rb_delta( 1, 1, 1000 )
+    pose.set_jump( pose.num_jump(), jump_control )
+    low_res_control = low_res_sfxn.score( pose )
+
+    jump.set_rb_delta( 1, 1, d_tx ) #TODO make deep copies so we don't need to undo this
+    pose.set_jump( pose.num_jump(), jump )
+    low_res_score = low_res_sfxn.score( pose ) - low_res_control
+    return low_res_score / 10 #TEMP
+
+    print( "low_res_score", low_res_score )
     if low_res_score >= 0.0:
         return magic_number_for_failed_docking_filter()
 
